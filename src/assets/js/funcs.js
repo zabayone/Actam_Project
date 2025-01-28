f183440176bdeec532681218138d44b1b1a00395
Nicola Nespoli <nespolinicola5@gmail.com>
2025-01-28 15:04:47 +0100

    updated button structure, still dowsn't work

--- src/assets/js/funcs.js
+++ src/assets/js/funcs.js
@@ -134,6 +134,12 @@ function selectCategory(arg){
     if (arg === 4){
         document.location.href = '/ear-training/results.html'
     }
+    if (arg === 5){
+        document.location.href = '/arcade/flappy.html'
+    }
+    if (arg === 6){
+        document.location.href = '/arcade/guess.html'
+    }
     else {
         document.location.href = '/ear-training/map.html'
     }
